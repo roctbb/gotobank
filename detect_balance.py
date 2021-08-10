6a5bf8676dfdc12a8a7d31512230e202a3af47ba
roctbb <roctbb@gmail.com>
2021-08-10 20:58:45 +0300

    add balance

--- detect_balance.py
+++ detect_balance.py
@@ -2,5 +2,5 @@ from bank_server import app
 from models import Account
 
 with app.app_context():
-    for account in Account.query.all():
+    for account in sorted(Account.query.all(), key=lambda x: x.balance()):
         print("{} {} - {}".format(account.name, account.surname, account.balance()))
